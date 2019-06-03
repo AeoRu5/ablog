3bc9c2ebc828e5d9945794890a0bea8eaaed2735
aeorus <2297660552@qq.com>
2019-06-03 15:36:16 +0800

    注册加盐

--- server/app/service/getSignInResult.js
+++ server/app/service/getSignInResult.js
@@ -1,70 +1,82 @@
 'use strict';
 
-const Service = require('egg').Service;
+const Service = require('egg').Service,
+	MD5 = require('md5.js');
 
 class GetSignInResultService extends Service {
-  async post(params) {
-    try {
-      let SignInResult;
-      const {
-        tel,
-        password
-      } = params,
-      selectUserResult = await this.app.mysql.get('users', {
-        tel,
-        enabled: 1
-      });
+	async post(params) {
+		try {
+			let SignInResult;
+			const {
+				tel,
+				password
+			} = params,
+			selectUserResult = await this.app.mysql.get('users', {
+				tel,
+				enabled: 1
+			});
 
-      if (selectUserResult && selectUserResult.enabled == 1) {
-        if (selectUserResult.password == password) {
-          const updateUserLoadStatusResult = await this.app.mysql.update('users', {
-              loaded: 1
-            }, {
-              where: {
-                userid: selectUserResult.userid
-              }
-            }),
-            updateUserLoadStatusSuccess = updateUserLoadStatusResult.affectedRows === 1;
+			if (selectUserResult && selectUserResult.enabled == 1) {
+				const selectSaleResult = await this.app.mysql.get('verification', {
+					userid: selectUserResult.userid
+				}),
+				salt = selectSaleResult.salt,
+				oriPasswordMD5 = new MD5().update(password).digest('hex'),
+				frontOriPasswordMD5 = oriPasswordMD5.substring(0, 16),
+				afterOriPasswordMD5 = oriPasswordMD5.substring(16, 32),
+				frontSalt = salt.substring(16, 32),
+				afterSalt = salt.substring(16, 32),
+				saltPassword = new MD5().update(afterOriPasswordMD5 + frontSalt + frontOriPasswordMD5 + afterSalt).digest('hex');
+				
+				if (selectUserResult.password == saltPassword) {
+					const updateUserLoadStatusResult = await this.app.mysql.update('users', {
+							loaded: 1
+						}, {
+							where: {
+								userid: selectUserResult.userid
+							}
+						}),
+						updateUserLoadStatusSuccess = updateUserLoadStatusResult.affectedRows === 1;
 
-          if (updateUserLoadStatusSuccess) {
-            this.ctx.session.USERINFO = {
-              loaded: 1,
-              tel: selectUserResult.tel,
-              userid: selectUserResult.userid,
-              nickname: selectUserResult.nickname,
-              createDate: selectUserResult.createDate
-            };
+					if (updateUserLoadStatusSuccess) {
+						this.ctx.session.USERINFO = {
+							loaded: 1,
+							tel: selectUserResult.tel,
+							userid: selectUserResult.userid,
+							nickname: selectUserResult.nickname,
+							createDate: selectUserResult.createDate
+						};
 
-            SignInResult = {
-              message: '登陆成功~',
-              success: true
-            }
-          } else {
-            SignInResult = {
-              message: '登陆失败请重试~',
-              success: false
-            }
-          }
-        } else {
-          SignInResult = {
-            message: '密码错误~',
-            success: false
-          }
-        }
-      } else {
-        SignInResult = {
-          message: '未注册该账号~',
-          success: false
-        }
-      }
+						SignInResult = {
+							message: '登陆成功~',
+							success: true
+						}
+					} else {
+						SignInResult = {
+							message: '登陆失败请重试~',
+							success: false
+						}
+					}
+				} else {
+					SignInResult = {
+						message: '密码错误~',
+						success: false
+					}
+				}
+			} else {
+				SignInResult = {
+					message: '未注册该账号~',
+					success: false
+				}
+			}
 
-      return SignInResult;
-    } catch (e) {
-      return Object.assign(e, {
-        success: false
-      });
-    }
-  }
+			return SignInResult;
+		} catch (e) {
+			return Object.assign(e, {
+				success: false
+			});
+		}
+	}
 }
 
 module.exports = GetSignInResultService;
--- server/app/service/getSignUpResult.js
+++ server/app/service/getSignUpResult.js
@@ -1,53 +1,77 @@
 'use strict';
 
-const Service = require('egg').Service;
+const Service = require('egg').Service,
+	uuid = require('uuid'),
+	MD5 = require('md5.js');
 
 class GetSignUpResultService extends Service {
-  async post(params) {
-    try {
-      let SignUpResult;
-      const {
-        tel,
-        securityCode,
-        password
-      } = params,
-      selectUserResult = await this.app.mysql.get('users', {
-        tel,
-        enabled: 1
-      });
+	async post(params) {
+		try {
+			let SignUpResult;
+			const {
+				tel,
+				securityCode,
+				password
+			} = params,
+			selectUserResult = await this.app.mysql.get('users', {
+				tel,
+				enabled: 1
+			});
 
-      if (!selectUserResult || selectUserResult.enabled == 0) {
-        const insertUserResult = await this.app.mysql.insert('users', {
-            tel,
-            password
-          }),
-          insertUserSuccess = insertUserResult.affectedRows === 1;
+			if (!selectUserResult || selectUserResult.enabled == 0) {
+				const userid = uuid.v1().split('-').join(''),
+					salt = uuid.v4().split('-').join(''),
+					oriPasswordMD5 = new MD5().update(password).digest('hex'),
+					frontOriPasswordMD5 = oriPasswordMD5.substring(0, 16),
+					afterOriPasswordMD5 = oriPasswordMD5.substring(16, 32),
+					frontSalt = salt.substring(16, 32),
+					afterSalt = salt.substring(16, 32),
+					saltPassword = new MD5().update(afterOriPasswordMD5 + frontSalt + frontOriPasswordMD5 + afterSalt).digest('hex'),
+					insertUserResult = await this.app.mysql.insert('users', {
+						userid,
+						tel,
+						password: saltPassword
+					}),
+					insertUserSuccess = insertUserResult.affectedRows === 1;
 
-        if (insertUserSuccess) {
-          SignUpResult = {
-            message: '注册成功~',
-            success: true
-          };
-        } else {
-          SignUpResult = {
-            message: insertUserResult.message,
-            success: false
-          };
-        }
-      } else {
-        SignUpResult = {
-          message: '该手机号已被注册啦~',
-          success: false
-        }
-      }
+				if (insertUserSuccess) {
+					const insertPasswordResult = await this.app.mysql.insert('verification', {
+							userid,
+							salt
+						}),
+						insertPasswordSuccess = insertPasswordResult.affectedRows === 1;
 
-      return SignUpResult;
-    } catch (e) {
-      return Object.assign(e, {
-        success: false
-      });
-    }
-  }
+					if (insertPasswordSuccess) {
+						SignUpResult = {
+							message: '注册成功~',
+							success: true
+						};
+					} else {
+						SignUpResult = {
+							message: '注册失败请重试~',
+							success: false
+						};
+					}
+				} else {
+					SignUpResult = {
+						message: insertUserResult.message,
+						success: false
+					};
+				}
+			} else {
+				SignUpResult = {
+					message: '该手机号已被注册啦~',
+					success: false
+				}
+			}
+
+			return SignUpResult;
+		} catch (e) {
+			return Object.assign(e, {
+				success: false
+			});
+		}
+	}
 }
 
 module.exports = GetSignUpResultService;
